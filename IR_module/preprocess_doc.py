f0756180366bbfcc30fa63a9455db1656839ccb2
EllNj <ellisnjie@live.co.uk>
2024-07-08 22:07:08 +0100

    Initial Commit

---
+++ IR_module/preprocess_doc.py
@@ -0,0 +1,28 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from PyPDF2 import PdfReader
+import os
+
+
+def text_from_pdf(pdf_path):# im thinking that to implement this in a web app, maybe generate ID
+    text = ''
+    with open(pdf_path, "rb") as file:
+        reader = PdfReader(file)
+        for num in range(len(reader.pages)):
+            text += reader.pages[num].extract_text()
+    return text
+
+def create_chunks(text, chunk_size=512, overlap=60):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=overlap)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+document = '../uploads/testing.pdf'
+
+text = text_from_pdf(document)
+
+chunks = create_chunks(text)
+print(chunks[2])
+
+
+def text_to_chunks(pdf_path):
+    return create_chunks(text_from_pdf(pdf_path))
---
+++ IR_module/vectorising.py
@@ -0,0 +1,23 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+import numpy as np
+from preprocess_doc import text_to_chunks
+from langchain.schema import Document
+
+document = '../uploads/testing.pdf'
+chunks = text_to_chunks(document)
+documents = [Document(page_content=chunk)for chunk in chunks]
+
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+
+db = FAISS.from_documents(documents, embedding_model)
+
+print(db.index.ntotal)
+
+query = "would my AI that sees if someone has good credit be considered high risk?"
+docs = db.similarity_search(query)
+print(f"this is chunk 1, {docs[0].page_content}")
+print(f"this is chunk 2, {docs[1].page_content}")
+print(f"this is chunk 3, {docs[2].page_content}")
+
